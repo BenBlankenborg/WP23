c4250e7e9618d2f1ca5f282db290398d85867a7e
BenBlankenborg <b.b.g.blankenborg@student.rug.nl>
2023-05-26 17:23:22 +0200

    Add main.js script and templates to index

--- assignment_3/index.php
+++ assignment_3/index.php
@@ -10,3 +10,15 @@ $navigation = Array(
         'Simple Form' => '/WP23/assignment_3/simple_form.php'
     )
 );
+include __DIR__ . '/tpl/head.php';
+include __DIR__ . '/tpl/body_start.php';
+?>
+<script type="application/javascript" src="scripts/main.js"></script>
+<div class="pd-40"></div>
+<div class="row">
+    <div class="col-md-12" id="news_container">
+    </div>
+</div>
+<?php
+include __DIR__ . '/tpl/body_end.php';
+?>
