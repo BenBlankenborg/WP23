e204a941f1a595bf6694b6a3063a2eabe3f084dd
BenBlankenborg <b.b.g.blankenborg@student.rug.nl>
2023-05-26 18:01:16 +0200

    Add templates and form for new articles

--- assignment_3/news_add.php
+++ assignment_3/news_add.php
@@ -10,3 +10,22 @@ $navigation = Array(
         'Simple Form' => '/WP23/assignment_3/simple_form.php'
     )
 );
+include __DIR__ . '/tpl/head.php';
+include __DIR__ . '/tpl/body_start.php';
+?>
+
+<form action="scripts/add_item.php" method="POST">
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" name="title">
+    </div>
+    <div class="form-group">
+        <label for="article">Article</label>
+        <textarea class="form-control" id="article" name="article" rows="3"></textarea>
+    </div>
+    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+
+<?php
+include __DIR__ . '/tpl/body_end.php';
+?>
